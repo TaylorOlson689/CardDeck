0f6056a3525e8a7b08bad2c38c9dae349cacd890
Taylor Olson <taylolso689@ed.amdsb.ca>
2023-02-16 09:31:17 -0500

    FEB 16

--- CardDeck/Form1.cs
+++ CardDeck/Form1.cs
@@ -12,20 +12,25 @@ namespace CardDeck
 {
     public partial class Form1 : Form
     {
+        Random randGen = new Random();
+
         // deck of cards
         List<string> deck = new List<string>();
+        List<string> playerCards = new List<string>();
+        List<string> drawerCards = new List<string>();
+        List<string> deckTemp = new List<string>();
 
         public Form1()
         {
             InitializeComponent();
-
+            
             //fill deck with standard 52 cards
             //H - Hearts, D - Diamonds, C - Clubs, S - Spades
             
-            deck.AddRange(new string[] { "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH" });
-            deck.AddRange(new string[] { "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD" });
-            deck.AddRange(new string[] { "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC" });
-            deck.AddRange(new string[] { "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS" });
+            deck.AddRange(new string[] {"2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH" });
+            deck.AddRange(new string[] {"2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD" });
+            deck.AddRange(new string[] {"2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC" });
+            deck.AddRange(new string[] {"2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS" });
 
             ShowDeck();
         }
@@ -37,7 +42,6 @@ namespace CardDeck
 
         private void shuffleButton_Click(object sender, EventArgs e)
         {
-            List<string> deckTemp = new List<string>();
             Random randGen = new Random();
 
             while (deck.Count > 0)
@@ -49,17 +53,30 @@ namespace CardDeck
 
             deck = deckTemp;
 
+            outputLabel.Text = $"";
+            playerCardsLabel.Text = $"Shuffling...";
+            dealerCardsLabel.Text = $"Shuffling...";
+            outputLabel.Text = "2H, 3H, 4H, 5H, 6H, 7H, 8H, 9H, 10H, JH, QH, KH, AH, 2D, 3D, 4D, 5D, 6D, 7D, 8D, 9D, 10D, JD, QD, KD, AD, 2C, 3C, 4C, 5C, 6C, 7C, 8C, 9C, 10C, JC, QC, KC, AC, 2S,3S, 4S, 5S, 6S, 7S, 8S, 9S, 10S, JS, QS, KS, AS";
+
             ShowDeck();
         }
 
         private void dealButton_Click(object sender, EventArgs e)
         {
+            playerCardsLabel.Text = $"{deckTemp}";
+            dealerCardsLabel.Text = $"{deckTemp}";
+            outputLabel.Text = $"{deckTemp}";
 
+            ShowDeck();
         }
 
         private void collectButton_Click(object sender, EventArgs e)
         {
+            playerCardsLabel.Text = $"Collecting...";
+            dealerCardsLabel.Text = $"Collecting...";
+            outputLabel.Text = $"2H, 3H, 4H, 5H, 6H, 7H, 8H, 9H, 10H, JH, QH, KH, AH, 2D, 3D, 4D, 5D, 6D, 7D, 8D, 9D, 10D, JD, QD, KD, AD, 2C, 3C, 4C, 5C, 6C, 7C, 8C, 9C, 10C, JC, QC, KC, AC, 2S,3S, 4S, 5S, 6S, 7S, 8S, 9S, 10S, JS, QS, KS, AS";
 
+            ShowDeck();
         }
     }
 }
