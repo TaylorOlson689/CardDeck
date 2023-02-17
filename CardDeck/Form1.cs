f83c8b7bec218c16cdc7e685c6107c22189a51f7
Taylor Olson <taylolso689@ed.amdsb.ca>
2023-02-17 09:39:11 -0500

    feb 17

--- CardDeck/Form1.cs
+++ CardDeck/Form1.cs
@@ -95,7 +95,7 @@ namespace CardDeck
         {
             playerCardsLabel.Text = $"";
             dealerCardsLabel.Text = $"";
-            outputLabel.Text = $"";
+            ShowDeck();
         }
     }
 }
